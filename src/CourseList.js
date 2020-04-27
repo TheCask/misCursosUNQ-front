d8eb624855b64495d856c7d4ddb604984008c9d6
TheCask <eugeniocalcena@gmail.com>
2020-04-27 20:29:30 -0300

    icons - tooltips on course list,  modal on delete

--- src/CourseList.js
+++ src/CourseList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Button, ButtonGroup, Container, Table, Spinner, UncontrolledTooltip } from 'reactstrap';
+import { Button, ButtonGroup, Container, Table, Spinner, 
+  UncontrolledTooltip, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import AppNavbar from './AppNavbar';
@@ -9,9 +10,9 @@ class CourseList extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {courses: [], isLoading: true, tooltipOpen: false};
+    this.state = {courses: [], isLoading: true, modal: false};
     this.remove = this.remove.bind(this);
-    this.toggle = this.toggle.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
   }
 
   componentDidMount() {
@@ -36,7 +37,7 @@ class CourseList extends Component {
   }
 
   render() {
-    const {courses, isLoading} = this.state;
+    const {courses, isLoading, modal} = this.state;
     if (isLoading) {
       return <div><Spinner style={{ width: '2rem', height: '2rem', marginLeft: '50%', marginTop: '50%'  }} color="danger" /> </div>
     }
@@ -52,22 +53,52 @@ class CourseList extends Component {
           <ButtonGroup inline="true">
             <Button size="sm" color="primary" tag={Link} to={"/course/" + course.courseId} id="editCourse">
               <UncontrolledTooltip placement="auto" target="editCourse">
-                Edit course
+                Edit Course
               </UncontrolledTooltip>
               <FontAwesomeIcon icon={['fas', 'edit']} size="1x"/>
             </Button>
-            <Button size="sm" color="danger" onClick={() => this.remove(course.courseId)} id="deleteCourse">
+            <Button size="sm" color="danger" onClick={this.toggleModal} id="deleteCourse">
               <UncontrolledTooltip placement="auto" target="deleteCourse">
-                Delete course
+                Delete Course
               </UncontrolledTooltip>
               <FontAwesomeIcon icon={['fas', 'minus-circle']} size="1x"/>
             </Button>
+
+            <Modal isOpen={modal} toggle={this.toggleModal} size="lg">
+              <ModalHeader toggle={this.toggleModal}><h3>Are you sure you want to delete entire course?</h3></ModalHeader>
+              <ModalBody>
+                <h4> This action will have the following consequences:</h4>
+                <ul>
+                  <li>- The course will no longer be alvailable
+                  <br></br>
+                  (Students will exit the course)</li>
+                  <br></br>
+                  <li>- Course lessons will no longer be alvailable
+                  <br></br>
+                  (Students will lost their attendance to this course)</li>
+                </ul>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" onClick={() => {this.remove(course.courseId); this.toggleModal()}} id="modalDeleteCourse">
+                  <UncontrolledTooltip placement="auto" target="modalDeleteCourse">
+                    YES DELETE COURSE (I'm Pretty Sure)
+                  </UncontrolledTooltip>
+                  <FontAwesomeIcon icon={['fas', 'minus-circle']} size="2x"/>
+                </Button>
+                <Button color="secondary" onClick={this.toggleModal} id="modalCancel">
+                  <UncontrolledTooltip placement="auto" target="modalCancel">
+                    Cancel and Back to Course
+                  </UncontrolledTooltip>
+                  <FontAwesomeIcon icon={['fas', 'backward']} size="2x"/>
+                </Button>
+              </ModalFooter>
+            </Modal>
+
             <Button size="sm" color="success" tag={Link} to={`/course/${course.courseId}/lessons`} id="attendanceCourse">
               <UncontrolledTooltip placement="auto" target="attendanceCourse">
-                Take attendance
+                Take Attendance
               </UncontrolledTooltip>
               <FontAwesomeIcon icon={['fas', 'user-check']} size="1x"/>
-
             </Button>
           </ButtonGroup>
         </td>
@@ -83,9 +114,6 @@ class CourseList extends Component {
               <UncontrolledTooltip placement="auto" target="addCourseTooltip">
                 Add a course
               </UncontrolledTooltip>
-              {/* <Tooltip placement="auto" isOpen={this.tooltipOpen} target="addCourseTooltip" toggle={this.toggle}>
-                Add a course
-              </Tooltip> */}
               <FontAwesomeIcon icon={['fas', 'plus-circle']} size="2x"/>
             </Button>
           </div>
@@ -115,8 +143,9 @@ class CourseList extends Component {
     return value===false ? 'No' : 'Yes';
   }
   
-  toggle(e) {
-    this.setState({tooltipOpen: !this.state.tooltipOpen});
+  toggleModal(e) {
+    const modal = this.state.modal
+    this.setState({modal: !modal});
   } 
 
 }
